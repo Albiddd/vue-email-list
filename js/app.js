d5afc437dcf69beb7bbe3ba03b24d26f04dda13b
Albiddd <alberto.dore93@gmail.com>
2022-09-01 14:27:00 +0200

    email list

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+const app = new Vue({
+    el: '#app',
+    data:{
+        mailArray: [],
+    },
+    methods:{
+        fetchMail(){
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    this.mailArray.push(res.data.response)
+                })
+            }
+        }
+    },
+    mounted(){
+        this.fetchMail()
+    }
+})
